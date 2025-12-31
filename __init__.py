9fd1d735b26c2efd41c068b9bde1370c0fb49cdf
Anil70135 <mandaanil452@gmail.com>
2025-12-31 21:35:56 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import azure.functions as func
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    name = req.params.get('name')
-    if not name:
-        return func.HttpResponse("Hello, Team! This is our live demo function.", status_code=200)
-    return func.HttpResponse(f"Hello, {name}! Welcome to the Azure Function demo.", status_code=200)
